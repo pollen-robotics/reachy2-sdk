efd2b1c0d7982b0436c4554f635e4f95ba923619
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2023-09-14 10:36:00 +0200

    Start base info.

---
+++ reachy_sdk/base_info.py
@@ -0,0 +1,16 @@
+# from reachy_sdk import ReachySDK
+# from reachy_sdk_api.config_pb2 import ConfigReachy
+
+
+# class BaseInfo():
+#     def __init__(reachy: ReachySDK, config_msg: ConfigReachy) -> None:
+#         pass
+#         # self.reachy = reachy
+#         # self.grpc_config_msg = config
+
+#     def get_base_info(self):
+#         self.reachy.base_info.ip_address = ''
+#         self.reachy.base_info.robot_serial_number = self.grpc_config_msg.robot_serial_number
+#         self.reachy.base_info.robot_serial_number = self.grpc_config_msg.mobile_base_serial_number
+#         self.reachy.base_info.config = self.grpc_config_msg.config
+#         # self.reachy.base_info = self.grpc_config_msg.
