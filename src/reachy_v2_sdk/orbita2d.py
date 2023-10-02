79ee506f038275a909e53e016a86cb6ea06cf77e
glannuzel <gaelle.lannuzel@gmail.com>
2023-10-02 17:14:29 +0200

    Update all orbita registers.

--- src/reachy_v2_sdk/orbita2d.py
+++ src/reachy_v2_sdk/orbita2d.py
@@ -2,12 +2,12 @@ from grpc import Channel
 
 from reachy_sdk_api_v2.orbita2d_pb2 import (
     Axis,
-    Orbita2DField,
-    Orbita2DStateRequest,
+    # Orbita2DField,
+    # Orbita2DStateRequest,
     Orbita2DState,
 )
 
-from reachy_sdk_api_v2.component_pb2 import ComponentId
+# from reachy_sdk_api_v2.component_pb2 import ComponentId
 from reachy_sdk_api_v2.orbita2d_pb2_grpc import Orbita2DServiceStub
 
 from .orbita_utils import OrbitaAxis
@@ -30,45 +30,64 @@ class Orbita2d:
 
     # TODO: perform the update in a thread
     # TODO: find a smarter way to do this
-    def update_2dstate(self) -> None:
-        resp = self._stub.GetState(
-            Orbita2DStateRequest(
-                id=ComponentId(id=self.name),
-                fields=[
-                    Orbita2DField.PRESENT_POSITION,
-                    Orbita2DField.PRESENT_SPEED,
-                    Orbita2DField.PRESENT_LOAD,
-                    Orbita2DField.TEMPERATURE,
-                    Orbita2DField.GOAL_POSITION,
-                    Orbita2DField.SPEED_LIMIT,
-                    Orbita2DField.TORQUE_LIMIT,
-                ],
-            )
-        )
-        axis1_attr = getattr(self, self._axis1)
-        axis2_attr = getattr(self, self._axis2)
+    # def update_2dstate(self) -> None:
+    #     resp = self._stub.GetState(
+    #         Orbita2DStateRequest(
+    #             id=ComponentId(id=self.name),
+    #             fields=[
+    #                 Orbita2DField.PRESENT_POSITION,
+    #                 Orbita2DField.PRESENT_SPEED,
+    #                 Orbita2DField.PRESENT_LOAD,
+    #                 Orbita2DField.TEMPERATURE,
+    #                 Orbita2DField.GOAL_POSITION,
+    #                 Orbita2DField.SPEED_LIMIT,
+    #                 Orbita2DField.TORQUE_LIMIT,
+    #             ],
+    #         )
+    #     )
+    #     axis1_attr = getattr(self, self._axis1)
+    #     axis2_attr = getattr(self, self._axis2)
 
-        axis1_attr._present_position = resp.present_position.axis_1
-        axis2_attr._present_position = resp.present_position.axis_2
+    #     axis1_attr._present_position = resp.present_position.axis_1
+    #     axis2_attr._present_position = resp.present_position.axis_2
 
-        axis1_attr._present_speed = resp.present_speed.axis_1
-        axis2_attr._present_speed = resp.present_speed.axis_2
+    #     axis1_attr._present_speed = resp.present_speed.axis_1
+    #     axis2_attr._present_speed = resp.present_speed.axis_2
 
-        axis1_attr._present_load = resp.present_load.axis_1
-        axis2_attr._present_load = resp.present_load.axis_2
+    #     axis1_attr._present_load = resp.present_load.axis_1
+    #     axis2_attr._present_load = resp.present_load.axis_2
 
-        axis1_attr._goal_position = resp.goal_position.axis_1
-        axis2_attr._goal_position = resp.goal_position.axis_2
+    #     axis1_attr._goal_position = resp.goal_position.axis_1
+    #     axis2_attr._goal_position = resp.goal_position.axis_2
 
-        axis1_attr._speed_limit = resp.speed_limit.axis_1
-        axis2_attr._speed_limit = resp.speed_limit.axis_2
+    #     axis1_attr._speed_limit = resp.speed_limit.axis_1
+    #     axis2_attr._speed_limit = resp.speed_limit.axis_2
 
-        axis1_attr._torque_limit = resp.torque_limit.axis_1
-        axis2_attr._torque_limit = resp.torque_limit.axis_2
+    #     axis1_attr._torque_limit = resp.torque_limit.axis_1
+    #     axis2_attr._torque_limit = resp.torque_limit.axis_2
 
     def _update_with(self, new_state: Orbita2DState) -> None:
         """Update the orbita with a newly received (partial) state received from the gRPC server."""
         axis1_attr = getattr(self, self._axis1)
         axis2_attr = getattr(self, self._axis2)
+
         axis1_attr._temperature = new_state.temperature.axis_1
         axis2_attr._temperature = new_state.temperature.axis_2
+
+        axis1_attr._present_position = new_state.present_position.axis_1
+        axis2_attr._present_position = new_state.present_position.axis_2
+
+        axis1_attr._present_speed = new_state.present_speed.axis_1
+        axis2_attr._present_speed = new_state.present_speed.axis_2
+
+        axis1_attr._present_load = new_state.present_load.axis_1
+        axis2_attr._present_load = new_state.present_load.axis_2
+
+        axis1_attr._goal_position = new_state.goal_position.axis_1
+        axis2_attr._goal_position = new_state.goal_position.axis_2
+
+        axis1_attr._speed_limit = new_state.speed_limit.axis_1
+        axis2_attr._speed_limit = new_state.speed_limit.axis_2
+
+        axis1_attr._torque_limit = new_state.torque_limit.axis_1
+        axis2_attr._torque_limit = new_state.torque_limit.axis_2
--- src/reachy_v2_sdk/orbita3d.py
+++ src/reachy_v2_sdk/orbita3d.py
@@ -1,10 +1,11 @@
 from grpc import Channel
-from reachy_sdk_api_v2.orbita3d_pb2 import (
-    Orbita3DField,
-    Orbita3DStateRequest,
-)
 
-from reachy_sdk_api_v2.component_pb2 import ComponentId
+# from reachy_sdk_api_v2.orbita3d_pb2 import (
+#     Orbita3DField,
+#     Orbita3DStateRequest,
+# )
+
+# from reachy_sdk_api_v2.component_pb2 import ComponentId
 from reachy_sdk_api_v2.orbita3d_pb2_grpc import Orbita3DServiceStub
 from reachy_sdk_api_v2.orbita3d_pb2 import Orbita3DState
 from .orbita_utils import OrbitaAxis
@@ -23,48 +24,72 @@ class Orbita3d:
 
     # TODO: perform the update in a thread
     # TODO: find a smarter way to do this
-    def update_3dstate(self) -> None:
-        resp = self._stub.GetState(
-            Orbita3DStateRequest(
-                id=ComponentId(id=self.name),
-                fields=[
-                    Orbita3DField.PRESENT_POSITION,
-                    Orbita3DField.PRESENT_SPEED,
-                    Orbita3DField.PRESENT_LOAD,
-                    Orbita3DField.TEMPERATURE,
-                    Orbita3DField.GOAL_POSITION,
-                    Orbita3DField.SPEED_LIMIT,
-                    Orbita3DField.TORQUE_LIMIT,
-                ],
-            )
-        )
+    # def update_3dstate(self) -> None:
+    #     resp = self._stub.GetState(
+    #         Orbita3DStateRequest(
+    #             id=ComponentId(id=self.name),
+    #             fields=[
+    #                 Orbita3DField.PRESENT_POSITION,
+    #                 Orbita3DField.PRESENT_SPEED,
+    #                 Orbita3DField.PRESENT_LOAD,
+    #                 Orbita3DField.TEMPERATURE,
+    #                 Orbita3DField.GOAL_POSITION,
+    #                 Orbita3DField.SPEED_LIMIT,
+    #                 Orbita3DField.TORQUE_LIMIT,
+    #             ],
+    #         )
+    #     )
 
-        self.roll._present_position = resp.present_position.roll
-        self.pitch._present_position = resp.present_position.pitch
-        self.yaw._present_position = resp.present_position.yaw
+    #     self.roll._present_position = resp.present_position.roll
+    #     self.pitch._present_position = resp.present_position.pitch
+    #     self.yaw._present_position = resp.present_position.yaw
 
-        self.roll._present_speed = resp.present_speed.roll
-        self.pitch._present_speed = resp.present_speed.pitch
-        self.yaw._present_speed = resp.present_speed.yaw
+    #     self.roll._present_speed = resp.present_speed.roll
+    #     self.pitch._present_speed = resp.present_speed.pitch
+    #     self.yaw._present_speed = resp.present_speed.yaw
 
-        self.roll._present_load = resp.present_load.roll
-        self.pitch._present_load = resp.present_load.pitch
-        self.yaw._present_load = resp.present_load.yaw
+    #     self.roll._present_load = resp.present_load.roll
+    #     self.pitch._present_load = resp.present_load.pitch
+    #     self.yaw._present_load = resp.present_load.yaw
 
-        self.roll._goal_position = resp.goal_position.roll
-        self.pitch._goal_position = resp.goal_position.pitch
-        self.yaw._goal_position = resp.goal_position.yaw
+    #     self.roll._goal_position = resp.goal_position.roll
+    #     self.pitch._goal_position = resp.goal_position.pitch
+    #     self.yaw._goal_position = resp.goal_position.yaw
 
-        self.roll._speed_limit = resp.speed_limit.roll
-        self.pitch._speed_limit = resp.speed_limit.pitch
-        self.yaw._speed_limit = resp.speed_limit.yaw
+    #     self.roll._speed_limit = resp.speed_limit.roll
+    #     self.pitch._speed_limit = resp.speed_limit.pitch
+    #     self.yaw._speed_limit = resp.speed_limit.yaw
 
-        self.roll._torque_limit = resp.torque_limit.roll
-        self.pitch._torque_limit = resp.torque_limit.pitch
-        self.yaw._torque_limit = resp.present_position.yaw
+    #     self.roll._torque_limit = resp.torque_limit.roll
+    #     self.pitch._torque_limit = resp.torque_limit.pitch
+    #     self.yaw._torque_limit = resp.present_position.yaw
 
     def _update_with(self, new_state: Orbita3DState) -> None:
         """Update the orbita with a newly received (partial) state received from the gRPC server."""
         self.roll._temperature = new_state.temperature.roll
         self.pitch._temperature = new_state.temperature.pitch
         self.yaw._temperature = new_state.temperature.yaw
+
+        self.roll._present_position = new_state.present_position.roll
+        self.pitch._present_position = new_state.present_position.pitch
+        self.yaw._present_position = new_state.present_position.yaw
+
+        self.roll._present_speed = new_state.present_speed.roll
+        self.pitch._present_speed = new_state.present_speed.pitch
+        self.yaw._present_speed = new_state.present_speed.yaw
+
+        self.roll._present_load = new_state.present_load.roll
+        self.pitch._present_load = new_state.present_load.pitch
+        self.yaw._present_load = new_state.present_load.yaw
+
+        self.roll._goal_position = new_state.goal_position.roll
+        self.pitch._goal_position = new_state.goal_position.pitch
+        self.yaw._goal_position = new_state.goal_position.yaw
+
+        self.roll._speed_limit = new_state.speed_limit.roll
+        self.pitch._speed_limit = new_state.speed_limit.pitch
+        self.yaw._speed_limit = new_state.speed_limit.yaw
+
+        self.roll._torque_limit = new_state.torque_limit.roll
+        self.pitch._torque_limit = new_state.torque_limit.pitch
+        self.yaw._torque_limit = new_state.present_position.yaw
