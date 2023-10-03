97bcaabe92f54e666a2d222da0f7c8d014b6ad9d
glannuzel <gaelle.lannuzel@gmail.com>
2023-10-03 18:04:09 +0200

    Setup head

--- src/reachy_v2_sdk/head.py
+++ src/reachy_v2_sdk/head.py
@@ -12,6 +12,9 @@ from reachy_sdk_api_v2.head_pb2_grpc import HeadServiceStub
 from reachy_sdk_api_v2.head_pb2 import Head as Head_proto
 from reachy_sdk_api_v2.part_pb2 import PartId
 
+from .orbita3d import Orbita3d
+from .dynamixel_motor import DynamixelMotor
+
 
 class Head:
     """Head class.
@@ -21,10 +24,28 @@ class Head:
     expressed in Reachy's coordinate system.
     """
 
-    def __init__(self, head: Head_proto, grpc_channel: grpc.Channel) -> None:
+    def __init__(self, head_msg: Head_proto, grpc_channel: grpc.Channel) -> None:
         """Set up the head."""
+        self._grpc_channel = grpc_channel
         self._head_stub = HeadServiceStub(grpc_channel)
-        self.part_id = PartId(id=head.part_id)
+        self.part_id = PartId(id=head_msg.part_id.id, name=head_msg.part_id.name)
+
+        self._setup_head(head_msg)
+
+    def _setup_head(self, head: Head_proto) -> None:
+        description = head.description
+        self.neck = Orbita3d(
+            name=description.neck.id.id,
+            grpc_channel=self._grpc_channel,
+        )
+        self.l_antenna = DynamixelMotor(
+            name=description.l_antenna.id.id,
+            grpc_channel=self._grpc_channel,
+        )
+        self.r_antenna = DynamixelMotor(
+            name=description.r_antenna.id.id,
+            grpc_channel=self._grpc_channel,
+        )
 
     def look_at(self, x: float, y: float, z: float, duration: float) -> None:
         pass
