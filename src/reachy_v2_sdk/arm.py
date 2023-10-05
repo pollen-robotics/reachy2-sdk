250f9a9fc02e2de4bbc6fe320b3aa4f6aac55e27
glannuzel <gaelle.lannuzel@gmail.com>
2023-10-05 16:57:23 +0200

    Add goto cartesian from euler

--- src/reachy_v2_sdk/arm.py
+++ src/reachy_v2_sdk/arm.py
@@ -1,17 +1,20 @@
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 import grpc
 
 import numpy as np
 import numpy.typing as npt
 
+from google.protobuf.wrappers_pb2 import FloatValue
+
 from reachy_sdk_api_v2.arm_pb2_grpc import ArmServiceStub
 from reachy_sdk_api_v2.arm_pb2 import Arm as Arm_proto, ArmPosition
-from reachy_sdk_api_v2.arm_pb2 import ArmJointGoal, ArmState
+from reachy_sdk_api_v2.arm_pb2 import ArmJointGoal, ArmState, ArmCartesianGoal
 from reachy_sdk_api_v2.arm_pb2 import ArmLimits, ArmTemperatures
 from reachy_sdk_api_v2.arm_pb2 import ArmFKRequest, ArmIKRequest, ArmEndEffector
 from reachy_sdk_api_v2.part_pb2 import PartId
-from reachy_sdk_api_v2.kinematics_pb2 import Matrix4x4
+from reachy_sdk_api_v2.kinematics_pb2 import Matrix4x4, Point, Rotation3D, EulerAngles
+from reachy_sdk_api_v2.kinematics_pb2 import PointDistanceTolerances, EulerAnglesTolerances
 
 from .orbita2d import Orbita2d
 from .orbita3d import Orbita3d
@@ -108,13 +111,33 @@ class Arm:
 
         return positions
 
-    # def goto_point(self, position: List[float], orientation: List[float],
-    #                position_tol: List[float], orientation_tol: List[float], duration: float) -> None:
-    #     goal = ArmCartesianGoal(duration=duration)
+    def goto(
+        self,
+        position: Tuple[float, float, float],
+        orientation: Tuple[float, float, float],
+        position_tol: Optional[Tuple[float, float, float]] = (0, 0, 0),
+        orientation_tol: Optional[Tuple[float, float, float]] = (0, 0, 0),
+        duration: float = 0,
+    ) -> None:
+        target = ArmCartesianGoal(
+            id=self.part_id,
+            target_position=Point(x=position[0], y=position[1], z=position[2]),
+            target_orientation=Rotation3D(rpy=EulerAngles(roll=orientation[0], pitch=orientation[1], yaw=orientation[2])),
+            duration=FloatValue(value=duration),
+        )
+        if position_tol is not None:
+            target.position_tolerance = PointDistanceTolerances(
+                x_tol=position_tol[0], y_tol=position_tol[1], z_tol=position_tol[2]
+            )
+        if orientation_tol is not None:
+            target.orientation_tolerance = EulerAnglesTolerances(
+                x_tol=orientation_tol[0], y_tol=orientation_tol[1], z_tol=orientation_tol[2]
+            )
+        self._arm_stub.GoToCartesianPosition(target)
 
     def goto_joints(self, positions: List[float], duration: float) -> None:
         arm_pos = self._list_to_arm_position(positions)
-        goal = ArmJointGoal(id=self.part_id, position=arm_pos, duration=duration)
+        goal = ArmJointGoal(id=self.part_id, position=arm_pos, duration=FloatValue(value=duration))
         self._arm_stub.GoToJointPosition(goal)
 
     @property
