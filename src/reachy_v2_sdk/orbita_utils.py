db2291263ab4509ccbc257d2f3566e83b33037ba
glannuzel <gaelle.lannuzel@gmail.com>
2023-10-09 16:42:05 +0200

    Begin modif of OrbitaJoints.

--- src/reachy_v2_sdk/orbita_utils.py
+++ src/reachy_v2_sdk/orbita_utils.py
@@ -5,16 +5,54 @@ from .register import Register
 
 class OrbitaJoint:
     present_position = Register(readonly=True, label="present_position")
-    present_speed = Register(readonly=True, label="present_speed")
-    present_load = Register(readonly=True, label="present_load")
-    temperature = Register(readonly=True, label="temperature")
     goal_position = Register(readonly=False, label="goal_position")
+
+    def __init__(self, initial_state: Dict[str, float], axis_type: str) -> None:
+        self.axis_type = axis_type
+
+        self._state = initial_state
+
+        for field in dir(self):
+            value = getattr(self, field)
+            if isinstance(value, Register):
+                value.label = field
+
+    def __getitem__(self, field: str) -> float:
+        return self._state[field]
+
+    def __setitem__(self, field: str, value: float) -> None:
+        self._state[field] = value
+
+
+class OrbitaMotor:
+    temperature = Register(readonly=True, label="temperature")
     speed_limit = Register(readonly=False, label="speed_limit")
     torque_limit = Register(readonly=False, label="torque_limit")
 
     def __init__(self, initial_state: Dict[str, float], axis_type: str) -> None:
         self.axis_type = axis_type
         self.pid = PID(p=0.0, i=0.0, d=0.0)
+
+        self._state = initial_state
+
+        for field in dir(self):
+            value = getattr(self, field)
+            if isinstance(value, Register):
+                value.label = field
+
+    def __getitem__(self, field: str) -> float:
+        return self._state[field]
+
+    def __setitem__(self, field: str, value: float) -> None:
+        self._state[field] = value
+
+
+class OrbitaAxis:
+    present_speed = Register(readonly=True, label="present_speed")
+    present_load = Register(readonly=True, label="present_load")
+
+    def __init__(self, initial_state: Dict[str, float], axis_type: str) -> None:
+        self.axis_type = axis_type
 
         self._state = initial_state
 
