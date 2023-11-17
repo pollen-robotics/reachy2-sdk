138c9bf433686502e5776bbf42be72f438a81aa3
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2023-11-17 15:02:24 +0100

    Add documentation, create setup_hand method and define opening, present and goal_position.

--- src/reachy2_sdk/hand.py
+++ src/reachy2_sdk/hand.py
@@ -1,26 +1,55 @@
+"""Reachy Hand module.
+
+
+Handles all specific method to a Hand:
+    - turn_on / turn_off
+    - open / close
+"""
 import grpc
 import numpy as np
-
-from google.protobuf.wrappers_pb2 import BoolValue, FloatValue
 from reachy2_sdk_api.hand_pb2 import Hand as Hand_proto
-from reachy2_sdk_api.hand_pb2 import HandPosition, HandPositionRequest, HandState, ParallelGripperPosition
-from reachy2_sdk_api.part_pb2 import PartId
+from reachy2_sdk_api.hand_pb2 import (
+    HandPosition,
+    HandPositionRequest,
+    HandState,
+    ParallelGripperPosition,
+)
 from reachy2_sdk_api.hand_pb2_grpc import HandServiceStub
-
-from typing import Any
+from reachy2_sdk_api.part_pb2 import PartId
 
 
 class Hand:
     def __init__(self, hand_msg: Hand_proto, initial_state: HandState, grpc_channel: grpc.Channel) -> None:
-        """Set up the arm with its kinematics."""
+        """Set up the hand."""
         self._hand_stub = HandServiceStub(grpc_channel)
-        self.type = "gripper"
-        self.part_id = PartId(id=hand_msg.part_id.id)
+
+        self._setup_hand(hand_msg, initial_state)
 
     def __repr__(self) -> str:
-        return ""
+        """Clean representation of a Hand."""
+        return f"Hand with part_id {self.part_id} and opening of {self.opening}%"
+
+    def _setup_hand(self, hand_msg: Hand_proto, initial_state: HandState) -> None:
+        """Set up the hand.
+
+        It will create the hand and set its initial state.
+        """
+        self.part_id = PartId(id=hand_msg.part_id.id)
+        self._present_position: float = round(np.rad2deg(initial_state.present_position.parallel_gripper.position), 1)
+        self._goal_position: float = round(np.rad2deg(initial_state.goal_position.parallel_gripper.position), 1)
+        self._opening: float = initial_state.opening.value
+
+    @property
+    def opening(self) -> float:
+        """Return the opening of the hand in percentage."""
+        return round(self._opening * 100, 2)
 
     def open(self, percentage: float = 100) -> None:
+        """Open the hand.
+
+        Args:
+            percentage (float): Percentage of the opening. Defaults to 100.
+        """
         if not 0.0 <= percentage <= 100.0:
             raise ValueError(f"Percentage should be between 0 and 100, not {percentage}")
 
@@ -30,11 +59,16 @@ class Hand:
             self._hand_stub.SetHandPosition(
                 HandPositionRequest(
                     id=self.part_id,
-                    position=HandPosition(parallel_gripper=ParallelGripperPosition(position=percentage/100.0))
+                    position=HandPosition(parallel_gripper=ParallelGripperPosition(position=percentage / 100.0)),
                 )
             )
 
     def close(self, percentage: float = 100) -> None:
+        """Close the hand.
+
+        Args:
+            percentage (float): Percentage of the closing. Defaults to 100.
+        """
         if not 0.0 <= percentage <= 100.0:
             raise ValueError(f"Percentage should be between 0 and 100, not {percentage}")
 
@@ -44,21 +78,26 @@ class Hand:
             self._hand_stub.SetHandPosition(
                 HandPositionRequest(
                     id=self.part_id,
-                    position=HandPosition(parallel_gripper=ParallelGripperPosition(position=(100-percentage)/100.0))
+                    position=HandPosition(parallel_gripper=ParallelGripperPosition(position=(100 - percentage) / 100.0)),
                 )
             )
 
     def turn_on(self) -> None:
+        """Turn all motors of the hand on.
+
+        All hand's motors will then be stiff.
+        """
         self._hand_stub.TurnOn(self.part_id)
 
     def turn_off(self) -> None:
+        """Turn all motors of the hand off.
+
+        All hand's motors will then be compliant.
+        """
         self._hand_stub.TurnOff(self.part_id)
 
     def _update_with(self, new_state: HandState) -> None:
-        for field, value in new_state.ListFields():
-            if isinstance(value, FloatValue):
-                setattr(self, field.name, value.value)
-            if isinstance(value, BoolValue):
-                setattr(self, field.name, value.value)
-            if isinstance(value, HandPosition):
-                setattr(self, field.name, value.parallel_gripper.position)
+        """Update the hand with a newly received (partial) state received from the gRPC server."""
+        self._present_position = new_state.present_position.parallel_gripper.position
+        self._goal_position = new_state.goal_position.parallel_gripper.position
+        self._opening = new_state.opening.value
