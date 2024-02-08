7173ca88609c6b23d1f6263dd9f72265642aa1cd
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2024-02-08 13:58:50 +0100

    enhancement #74: forget a _ in refactoring

--- tests/test_reachy_sdk.py
+++ tests/test_reachy_sdk.py
@@ -26,7 +26,7 @@ def test_unconnected() -> None:
         rsdk.audio
 
     with pytest.raises(AttributeError):
-        rsdk._cameras
+        rsdk.cameras
 
     assert len(rsdk.enabled_parts) == 0
 
