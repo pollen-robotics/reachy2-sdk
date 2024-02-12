491a6008ff0aa240e4190ba9a74da5783a3d1737
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2024-02-12 11:23:36 +0100

    remove french comments

--- src/reachy2_sdk/utils/custom_dict.py
+++ src/reachy2_sdk/utils/custom_dict.py
@@ -5,17 +5,15 @@ Defines a custom dict class to modify repr of dict.
 """
 from typing import Any, Generic, TypeVar
 
-# Définir un type générique pour les clés et les valeurs
-K = TypeVar("K", bound=Any)  # Clés de type str
-V = TypeVar("V", bound=Any)  # Valeurs de type str
+K = TypeVar("K", bound=Any)
+V = TypeVar("V", bound=Any)
 
 
 class CustomDict(dict[K, V], Generic[K, V]):
     """
-    Singleton pattern. Only one robot can be instancied by python kernel.
+    CustomDict for a nice display
     """
 
     def __repr__(self) -> str:
-        # Construire la représentation personnalisée ligne par ligne
         items = ",\n".join(f"'{k}': {v}" for k, v in self.items())
         return f"""{{{items}}}"""
