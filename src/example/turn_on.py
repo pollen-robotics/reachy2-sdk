1a85811ed82a308e29fc4dcf49d5c16f79c5446b
RemiFabre <remifabre1800@gmail.com>
2024-03-19 14:33:59 +0100

    Added test for: turn_off, move then turn_on

--- src/example/turn_on.py
+++ src/example/turn_on.py
@@ -17,6 +17,18 @@ def connect_to_reachy():
     return reachy
 
 
+def build_pose_matrix(x: float, y: float, z: float) -> npt.NDArray[np.float64]:
+    # The effector is always at the same orientation in the world frame
+    return np.array(
+        [
+            [0, 0, -1, x],
+            [0, 1, 0, y],
+            [1, 0, 0, z],
+            [0, 0, 0, 1],
+        ]
+    )
+
+
 def turn_on():
     reachy = connect_to_reachy()
 
@@ -35,25 +47,26 @@ def turn_on_debug():
 
     reachy.turn_on()
 
-    print("The arm should move now")
+    print("The arms should move now")
 
-    reachy.r_arm.elbow.pitch.goal_position = -90
-    time.sleep(1.0)
-    reachy.r_arm.elbow.pitch.goal_position = 0
+    pose = build_pose_matrix(0.3, -0.45, -0.3)
+    ik = reachy.r_arm.inverse_kinematics(pose)
+    reachy.r_arm.goto_joints(ik, 2.0, degrees=True)
+    pose = build_pose_matrix(0.3, 0.45, -0.3)
+    ik = reachy.l_arm.inverse_kinematics(pose)
+    reachy.l_arm.goto_joints(ik, 2.0, degrees=True)
+    reachy.head.rotate_to(np.pi / 3, np.pi / 3, np.pi / 3, duration=1.5, interpolation_mode="minimum_jerk", degrees=False)
+    time.sleep(2.0)
 
     print("The arm should NOT move now !")
-
     reachy.turn_off()
-    time.sleep(0.1)
-    reachy.r_arm.elbow.pitch.goal_position = -90
-    time.sleep(0.1)
-    reachy.turn_on()
+    for joint in reachy.joints.values():
+        joint.goal_position = 0
+    time.sleep(3.0)
 
-    time.sleep(1.0)
-    reachy.turn_off()
-    time.sleep(0.1)
+    print("The arm should NOT move now either !")
     reachy.turn_on()
-    time.sleep(0.1)
+    time.sleep(2.0)
 
     reachy.disconnect()
     ReachySDK.clear()
