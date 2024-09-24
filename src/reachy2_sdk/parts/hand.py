9177943055e5131c79c0cc47e7cddf2d1b19f0fa
glannuzel <gaelle.lannuzel@gmail.com>
2024-09-24 18:14:17 +0200

    Add is_motion_complete()

--- src/reachy2_sdk/parts/hand.py
+++ src/reachy2_sdk/parts/hand.py
@@ -5,9 +5,11 @@ Handles all specific method to a Hand:
     - turn_on / turn_off
     - open / close
 """
-from typing import Optional
+from collections import deque
+from typing import Deque, Optional
 
 import grpc
+import numpy as np
 from google.protobuf.wrappers_pb2 import FloatValue
 from reachy2_sdk_api.hand_pb2 import Hand as Hand_proto
 from reachy2_sdk_api.hand_pb2 import (
@@ -35,6 +37,11 @@ class Hand(Part):
         super().__init__(hand_msg, grpc_channel, HandServiceStub(grpc_channel))
         self._hand_stub = HandServiceStub(grpc_channel)
 
+        self._request_sent = False
+        self._request_reached = True
+        self._last_present_positions_queue_size = 10
+        self._last_present_positions: Deque[float] = deque(maxlen=self._last_present_positions_queue_size)
+
         self._setup_hand(hand_msg, initial_state)
 
     def __repr__(self) -> str:
@@ -51,6 +58,7 @@ class Hand(Part):
         self._goal_position: float = initial_state.goal_position.parallel_gripper.position.value
         self._opening: float = initial_state.opening.value
         self._compliant: bool = initial_state.compliant.value
+        self._last_present_positions.append(self._present_position)
 
         self._outgoing_goal_positions: Optional[float] = None
 
@@ -78,6 +86,8 @@ class Hand(Part):
                 ),
             )
         )
+        self._request_sent = True
+        self._request_reached = False
 
     @property
     def present_position(self) -> float:
@@ -111,12 +121,16 @@ class Hand(Part):
         if self._compliant:
             raise RuntimeError("Gripper is off. Open request not sent.")
         self._hand_stub.OpenHand(self._part_id)
+        self._request_sent = True
+        self._request_reached = False
 
     def close(self) -> None:
         """Close the hand."""
         if self._compliant:
             raise RuntimeError("Gripper is off. Close request not sent.")
         self._hand_stub.CloseHand(self._part_id)
+        self._request_sent = True
+        self._request_reached = False
 
     def is_on(self) -> bool:
         """Get compliancy of the hand"""
@@ -126,12 +140,29 @@ class Hand(Part):
         """Get compliancy of the hand"""
         return self._compliant
 
+    def is_motion_complete(self) -> bool:
+        """Get state of gripper movement"""
+        if not self._request_sent:
+            return True
+        else:
+            return self._request_reached
+
     def _update_with(self, new_state: HandState) -> None:
         """Update the hand with a newly received (partial) state received from the gRPC server."""
         self._present_position = new_state.present_position.parallel_gripper.position.value
         self._goal_position = new_state.goal_position.parallel_gripper.position.value
         self._opening = new_state.opening.value
         self._compliant = new_state.compliant.value
+        if self._request_sent:
+            if len(self._last_present_positions) < self._last_present_positions_queue_size:
+                pass
+            elif np.isclose(self._present_position, self._last_present_positions[-1], np.deg2rad(0.1)) and np.isclose(
+                self._present_position, self._last_present_positions[-2], np.deg2rad(0.1)
+            ):
+                self._request_reached = True
+                self._request_sent = False
+                self._last_present_positions.clear()
+            self._last_present_positions.append(self._present_position)
 
     def _update_audit_status(self, new_status: HandStatus) -> None:
         pass
