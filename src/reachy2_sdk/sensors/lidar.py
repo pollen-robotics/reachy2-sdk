dcf7831b36b344af8216ef8506c5c1d4510a2011
glannuzel <gaelle.lannuzel@gmail.com>
2024-10-08 12:27:21 +0200

    Review lidar.py docstrings

--- src/reachy2_sdk/sensors/lidar.py
+++ src/reachy2_sdk/sensors/lidar.py
@@ -45,11 +45,11 @@ class Lidar:
 
     def get_map(self) -> Optional[npt.NDArray[np.uint8]]:
         """
-        The function `get_map` retrieves the current map of the environment using lidar data.
+        Retrieves the current map of the environment using lidar data.
 
         Returns:
-          The `get_map` method returns the current map of the environment as an image (OpenCV format) if
-        the lidar map is successfully retrieved. If no lidar map is retrieved, it returns `None`.
+          current map of the environment as an image (numpy array) if the lidar map is successfully retrieved.
+        If no lidar map is retrieved, it returns `None`.
         """
         compressed_map = self._stub.GetLidarMap(self._part._part_id)
         if compressed_map.data == b"":
@@ -62,7 +62,7 @@ class Lidar:
     @property
     def safety_slowdown_distance(self) -> float:
         """
-        This function returns the safety distance in meters of the mobile base from obstacles.
+        Returns the safety distance from obstacles in meters of the mobile base.
 
         The mobile base's speed is slowed down if the direction of speed matches the direction of
         at least 1 LIDAR point in the safety_distance range.
@@ -75,11 +75,10 @@ class Lidar:
     @safety_slowdown_distance.setter
     def safety_slowdown_distance(self, value: float) -> None:
         """
-        This function sets the safety distance for a Lidar sensor.
+        Sets the safety distance for a Lidar sensor.
 
         Args:
-          value (float): The `value` parameter in the `safety_slowdown_distance` method represents the
-        safety distance that is being set for the LidarSafety object. It is of type float and is used to
+          value (float): safety distance that is being set for the LidarSafety object. It is of type float and is used to
         specify the distance at which a safety slowdown should be initiated.
         """
         self._stub.SetZuuuSafety(
@@ -91,7 +90,7 @@ class Lidar:
     @property
     def safety_critical_distance(self) -> float:
         """
-        This function returns the critical distance in meters of the mobile base from obstacles.
+        Returns the critical distance in meters of the mobile base from obstacles.
 
         The mobile base's speed is changed to 0 if the direction of speed matches the direction of
         at least 1 LIDAR point in the critical_distance range.
@@ -99,21 +98,19 @@ class Lidar:
         slowed down to the "safety_zone" speed.
 
         Returns:
-          The method `safety_critical_distance` returns the critical distance in meters of the mobile
-        base from obstacles as a float value.
+          the critical distance in meters of the mobile base from obstacles as a float value.
         """
         return float(self._critical_distance)
 
     @safety_critical_distance.setter
     def safety_critical_distance(self, value: float) -> None:
         """
-        The function `safety_critical_distance` sets the critical distance for a Lidar safety feature.
+        Sets the critical distance for a Lidar safety feature.
 
         Args:
-          value (float): The `value` parameter in the `safety_critical_distance` method represents the
-        critical distance for safety in the Lidar system. It is a floating-point value that specifies
-        the distance at which certain safety measures or actions should be taken to avoid potential
-        hazards or collisions.
+          value (float): critical distance in meters for safety. It specifies the distance at which the mobile base
+        should stop if trying to get in the direction of an obstacle. If at least 1 point is in the critical distance,
+        then even motions that move away from the obstacles are slowed down to the "safety_zone" speed.
         """
         self._stub.SetZuuuSafety(
             LidarSafety(
@@ -124,23 +121,21 @@ class Lidar:
     @property
     def safety_enabled(self) -> bool:
         """
-        The function `safety_enabled` returns the current status of the safety feature.
+        Returns the current status of the safety feature.
 
         Returns:
-          The method `safety_enabled` is returning the value of the `_safety_enabled` attribute, which
-        is a boolean indicating whether the safety feature is enabled or disabled.
+          a boolean indicating whether the safety feature is enabled or disabled. If `True`, the safety feature is enabled.
         """
         return self._safety_enabled
 
     @safety_enabled.setter
     def safety_enabled(self, value: bool) -> None:
         """
-        The function `safety_enabled` sets the safety status for a Lidar device.
+        Sets the safety status for the Lidar device.
 
         Args:
-          value (bool): The `value` parameter in the `safety_enabled` method is a boolean value that
-        indicates whether safety features are enabled or disabled. It is used to set the safety status
-        of the Lidar device.
+          value (bool): a boolean value that indicates whether safety features are enabled or disabled.
+        If `True`, the safety feature is enabled.
         """
         self._stub.SetZuuuSafety(
             LidarSafety(
@@ -151,8 +146,7 @@ class Lidar:
     @property
     def obstacle_detection_status(self) -> LidarObstacleDetectionStatus:
         """
-        This function returns the status of the lidar obstacle detection, which can be one of four
-        possible values.
+        Returns the status of the lidar obstacle detection, to know if an obstacle is in the LIDAR safety area.
 
         Returns:
           the status of the lidar obstacle detection, which can be one of the following values:
@@ -162,7 +156,7 @@ class Lidar:
 
     def reset_safety_default_values(self) -> None:
         """
-        The function `reset_safety_default_values` resets default distance values for safety detection.
+        Resets default distance values for safety detection.
 
         Reset values are:
         - safety_critical_distance
@@ -172,12 +166,10 @@ class Lidar:
 
     def _update_with(self, new_lidar_state: LidarSafety) -> None:
         """
-        The function `_update_with` updates lidar information with a new state received from a gRPC
-        server.
+        Updates lidar information with a new state received from a gRPC server.
 
         Args:
-          new_lidar_state (LidarSafety): `new_lidar_state` is an object of type `LidarSafety` that
-        contains information about the lidar state received from the gRPC server.
+          new_lidar_state (LidarSafety): contains information about the lidar state received from the gRPC server.
         """
         self._safety_enabled = new_lidar_state.safety_on.value
         self._safety_distance = new_lidar_state.safety_distance.value
