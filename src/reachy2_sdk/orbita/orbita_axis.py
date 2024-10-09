a059964ac91c530562693dc5c7e742e40733750f
glannuzel <gaelle.lannuzel@gmail.com>
2024-10-09 18:30:56 +0200

    orbita_axis.py dosctring

--- src/reachy2_sdk/orbita/orbita_axis.py
+++ src/reachy2_sdk/orbita/orbita_axis.py
@@ -21,18 +21,20 @@ class OrbitaAxis:
 
     @property
     def present_speed(self) -> float:
-        """
-        Get the present speed of the axis in rad/s.
-        """
+        """Get the present speed of the axis in radians per second."""
         return to_position(self._present_speed)
 
     @property
     def present_load(self) -> float:
-        """
-        Get the present load of the axis in N.
-        """
+        """Get the present load of the axis in Newtons."""
         return float(self._present_load)
 
     def _update_with(self, new_state: Dict[str, FloatValue]) -> None:
+        """Update the present speed and load of the axis with new state values.
+
+        Args:
+            new_state: A dictionary containing the new state values for the axis. The keys should include
+                "present_speed" and "present_load", with corresponding FloatValue objects as values.
+        """
         self._present_speed = new_state["present_speed"].value
         self._present_load = new_state["present_load"].value
