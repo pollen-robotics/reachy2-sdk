d01f817e2808edd39c98116ae26f476990def768
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2024-10-14 16:44:40 +0200

    documentation #422: fix example link

--- src/reachy2_sdk/__init__.py
+++ src/reachy2_sdk/__init__.py
@@ -13,7 +13,7 @@ reachy = ReachySDK(host="ip_address")
 
 And you're ready to use Reachy!
 
-*Examples and tutorials are available [here](../src/examples/)!*
+*Examples and tutorials are available [here](https://github.com/pollen-robotics/reachy2-sdk/tree/main/src/examples)!*
 
 """
 
